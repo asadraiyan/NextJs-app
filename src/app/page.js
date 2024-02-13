afd10c77de82679a9ab493f254d130fe02705a35
Asad Raiyan <asadraiyan001@gmail.com>
2024-02-13 09:55:29 +0530

    added next link in my app

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,24 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <nav className={styles.mainnav}>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Blog</li>
-          <li>Contact</li>
+          <Link href="/">
+            <li>Home</li>
+          </Link>
+          <Link href="/about">
+            <li>About</li>
+          </Link>
+          <Link href="/blog">
+            <li>Blog</li>
+          </Link>
+          <Link href="/contact">
+            <li>Contact</li>
+          </Link>
         </ul>
       </nav>
       <main className={styles.main}>
