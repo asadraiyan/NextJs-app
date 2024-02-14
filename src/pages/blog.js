e9224c7b6034d0ff615064e09a748ebf51d7ae40
Asad Raiyan <asadraiyan001@gmail.com>
2024-02-14 16:03:54 +0530

    created blogpost component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Link from "next/link";
+import styles from "../app/blog.module.css";
 
 const Blog = () => {
-  return <div>This is blog page</div>;
+  return (
+    <div>
+      <main className={styles.main}>
+        <div className={styles.blogItem}>
+          <Link href="/blogpost/learn-javascript" className={styles.link}>
+            <h3>How to learn Javascript in 2024</h3>
+          </Link>
+          <p>Javascript is the language used to design logic for the web</p>
+        </div>
+        <div className="blogItem">
+          <h3>How to learn Javascript in 2024</h3>
+          <p>Javascript is the language used to design logic for the web</p>
+        </div>
+        <div className="blogItem">
+          <h3>How to learn Javascript in 2024</h3>
+          <p>Javascript is the language used to design logic for the web</p>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Blog;
--- src/pages/blogpost/[slug].js
+++ src/pages/blogpost/[slug].js
@@ -1,10 +1,28 @@
 import React from "react";
 import { useRouter } from "next/router";
+import styles from "../../app/blogPost.module.css";
 
 const Slug = () => {
   const router = useRouter();
   const { slug } = router.query;
-  return <div>{slug}</div>;
+  return (
+    <>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1>Title of the page {slug}</h1>
+          <hr />
+          <div>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam
+            magni esse quam aspernatur, sit cupiditate quas? Pariatur, debitis
+            eveniet quae ipsum incidunt quas molestias necessitatibus ratione
+            ullam dignissimos iure suscipit? Eligendi, voluptatum delectus,
+            nobis mollitia, quas corporis animi dolorum unde dolores id quis
+            repellendus soluta odit! Fuga quas sed aperiam.
+          </div>
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default Slug;
